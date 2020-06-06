386b6c163d9f5bd069c4f621a28f6174ad0ddcad
iamsubingyawali <subingyawali7@gmail.com>
2020-06-06 15:59:26 +0545

    Improves UI

--- script.js
+++ script.js
@@ -7,17 +7,25 @@ function start(){
 }
 
 function execute(data){
-    console.log(data);
+    // console.log(data);
     document.querySelector("#photo img").src = data['avatar_url'];
     document.querySelector("#name p").innerHTML = data['name'];
     document.querySelector("#name span").innerHTML = data['bio'];
-    post('<i class="fas fa-user"></i>Username',data['login'],"#about ul");
-    post('<i class="fas fa-university"></i>College',data['company'],"#about ul");
-    post('<i class="fas fa-address-card"></i>Address',data['location'],"#about ul");
-    post('<i class="fas fa-blog"></i>Website',data['blog'],"#about ul")
-    post('<i class="fas fa-street-view"></i>Followers',data['followers'],"#about ul")
-    post('<i class="fas fa-running"></i>Following',data['following'],"#about ul")
+    post('<i style="color:#D9E770" class="fas fa-user"></i>Username',data['login'],"#about ul");
+    post('<i style="color:#4184F2" class="fas fa-university"></i>College',data['company'],"#about ul");
+    post('<i style="color:#F15B6C" class="fas fa-address-card"></i>Address',data['location'],"#about ul");
+    post('<i style="color:orange" class="fas fa-blog"></i>Website','<a target="_blank" href="'+data['blog']+'">'+data['blog']+'</a>',"#about ul")
+    post('<i style="color:#8B8FC1" class="fas fa-street-view"></i>Followers',data['followers'],"#about ul")
+    post('<i style="color:#2AC83A" class="fas fa-running"></i>Following',data['following'],"#about ul")
 
+    fetch(data['repos_url'])
+        .then(result => result.json())
+        .then(function(repos){
+            repos.forEach(repo => {
+                console.log(repo);
+                post('<i class="fas fa-folder-open"></i><a target="_blank" href="'+repo['html_url']+'">'+repo['name']+'</a>',repo['description'],"#repos ul");
+            });
+    });
 
     document.getElementById("loading").style.display = "none";
     document.getElementsByClassName("main")[0].style.display = "block";
@@ -26,12 +34,11 @@ function execute(data){
 function post(title,data,parent){
     let ul = document.querySelector(parent);
     let new_li = document.createElement("li");
-    let newText = document.createTextNode(data);
     let newSpan = document.createElement("span");
     ul.appendChild(new_li);
     new_li.appendChild(newSpan);
     newSpan.innerHTML = title;
-    new_li.appendChild(newText);
+    new_li.innerHTML+=data;
 }
 
 document.addEventListener('DOMContentLoaded',start);
